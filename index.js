ba2aa33ad7e17eb12fe5178f992872059ddbfd2e
AnnaLobunova <alobunova@gmail.com>
2024-05-14 22:51:34 +0300

    homework added

---
+++ index.js
@@ -0,0 +1,12 @@
+"use strict";
+const priceData = {
+    Apples: '23.4',
+    BANANAS: '48',
+    oRAngGEs: '48.7584',
+  };
+  
+  let updatedPriceData = {};
+  for (let key in priceData){
+    updatedPriceData[key]=priceData[key].toLowerCase;
+    console.log[updatedPriceData[key]];
+    };
